4f95b4ec17fe7cc658f2ce6882cb830f04138532
ganeshku73 <ganeshku73@gmail.com>
2021-04-10 11:12:21 +0530

    sandeep comit

---
+++ includes/_header.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
+<script type="text/javascript">
+
+function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+</script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script language="javascript" type="text/javascript" src="jquery_002.js"></script>
+<script src="swfobject_modified.js" type="text/javascript"></script>
+
+<link href="main.css" rel="stylesheet" type="text/css">
+<script src="pop_box.js" type="text/javascript"></script><script type="text/javascript" src="png.js"></script><link rel="stylesheet" type="text/css" href="ddsmoothmenu.css"><script type="text/javascript" src="jquery.js"></script><script type="text/javascript" src="ddsmoothmenu.js">
+
+</script><script type="text/javascript">
+
+ddsmoothmenu.init({
+mainmenuid: "smoothmenu1", //menu DIV id
+orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+classname: 'ddsmoothmenu', //class added to menu's outer DIV
+//customtheme: ["#1c5a80", "#18374a"],
+contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+</script><script type="text/jscript">
+function changeImage(filename)
+{
+	document.getElementById('blank').src = filename;
+}
+</script><script type="text/javascript">
+<!--
+function calc()
+{
+  //find the height of the internal page
+  var the_height1=
+    document.getElementById('blank').contentWindow.
+      document.body.scrollHeight;
+  //change the height of the iframe
+  document.getElementById('blank').height=
+      the_height1;
+}
+//-->
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Welcome To. M/s. Rana Pratap Construction & Earth Movers in Delhi | Earth Equipment in Delhi NCR| Rapid Building Demolition Rock Breaking Concrete Breaking Excavation | Dismantling in Delhi | Excavators Hiring Company in Delhi| M/s. Rana Pratap Construction 9871797901 in Delhi | Excavators Hire in Delhi | Rock Excavation Excavators Hiring in Delhi | M/s. Rana Pratap Construction & Earth Movers in Gurgaon | M/s. Rana Pratap Construction in delhi Faridabad | Earth Movers Machinery in Delhi NCR | M/s. Rana Pratap Construction in India| Excavators Hiring Company in India | Hard Rock Excavators Hiring Company in Faridabad | M/s. Rana Pratap Construction Hard Rock Excavation Company in Delhi | Earth Moving Machines in Delhi | M/s. Rana Pratap Construction & Earthmoving in Delhi | M/s. Rana Pratap Construction & Earthmoving and Hiring in Ghaziabad | M/s. Rana Pratap Construction & Earth Excavation Company in Delhi | Construction Equipment hiring Suppliers of India | Rental Construction Equipment Company | Rental Construction Equipment Company in Delhi | Rental Construction Equipment Company India | Earth Moving Machinery on Hire Basis | M/s. Rana Pratap Construction & Building Demolition in Delhi | M/s Rana Pratap Construction & Earth Movers in Uttranchal, Uttrakhand | M/s. Rana Pratap Construction Company in Uttrakhand | Construction & Earth Movers in Delhi | Constructions and Earth Movers | Hiring of Constructions Equipments | M/s. Rana Pratap Construction & Earthworks in Excavation of Soil in Delhi | Soft Rock Excavation in Delhi | Hard Rock Excavation in Delhi | Land Levelling filling compaction Disposal in Delhi NCR | Earthmoving Equipments on Rent in Gurgaon | Earth moving Equipments on Rent in Gurgaon | Earth moving Equipments on Rent in Delhi | Earth Moving Equipments on Rent in Noida | Earthmovers Equipments on Rent in Noida | Earthmovers Equipments on Rent in Delhi | Earthmovers Equipments on Rent in Gurgaon | Contractors earthmoving Machines in Delhi | Construction Earthmoving Machine in New Delhi | Tata EX200 Equipment Hire in Delhi NCR | Construction and Earthmoving Machinery in Delhi | M/s. Rana Pratap Constructions & Equipment Hiring Company In Delhi NCR India | Road Construction Equipments On Hire in Delhi | M/s Rana Pratap Construction Equipment Rental in Delhi India | Earth Mover and Excavators in Delhi | Earth Work and Excavators Company in Delhi | Excavators Hiring Company in Noida | Excavators Hiring Company in Gurgaon | Excavator Hiring in Gurgaon | Earth Movers Hiring in Gurgaon | Civil Engineering in Delhi | Building and Boundary Wall Construction Company in Jaipur | Agriculture Equipment in Chandigarh | Civil Contractors & Earthmoving and | All Type of Building Dismantling & Construction Company in Delhi | Rock Breaker Hiring in Delhi India | Hard Rock Work in Delhi | Poclain Dumpers & Hyva and Loaders Hiring in Delhi | Earth Disposal work in Delhi | land Laveling and Land Scraping Work in Delhi | Rock Breaking Concrete Breaking Excavation in Delhi | Hiring in Paschim Vihar New Delhi Delhi | Road Cutting and Construction Contractors in Delhi NCR | M/s. Rana Pratap Construction & Earth Movers Company In Delhi | Shifting of Earth in Delhi NCR | Rock Breaker Excavators Hiring in Delhi | Earth Movers and Excavators in Delhi Gurgaon | Excavators Hiring Company in Gurgaon Delhi NCR | Excavators Hiring in Gurgaon | Earth Movers Hiring in Gurgaon Noida | Earth Movers Machinery in Delhi Noida| Road Cutting and Construction Contractors in Delhi NCR | Hard rock Excavation in Himachal | Excavation Hiring Company in Himachal | Excavation Hiring Company in Uttaranchal | Earth moving contractors in Hardwar's | Earth moving contractors in Delhi Faridabad| Earth Moving Contractors in Gurgaon | Earth Moving Contractors in Uttaranchal | Earth Moving Contractors in Himachal | Civil Engineers and Contractors in Delhi</title>
+<link href="rana.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style3 {color: #CC9900}
+.style4 {color: #ececec}
+.style1 {font-weight: bold}
+.style2 {font-weight: bold}
+-->
+</style>
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+
+<div class="main">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="22">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div class="ddsmoothmenu" id="smoothmenu1">
+        <ul>
+          <li style="z-index: 91;"><a class=""  href="index.html">Home</a></li>
+          <li style="z-index: 91;"><a class=""  href="company-profile.html">Company Profile</a></li>
+          <li style="z-index: 91;"><a class="" href="our-clients.html">Our Clients</a></li>
+          <li style="z-index: 98;"><a class=""  href="#">Available Equipments</a>
+            <ul style="display: none; top: 29px; visibility: visible; left: 0px; width: 193px;">
+            <li><a href="excavator-hyundai-r215l-with-rock-breaker.html">Excavator Hyundai R215L with Rock Breaker</a></li>
+            <li><a href="excavator-hyundai-r140l.html">Excavator Hyundai R140L</a></li>
+              <li><a href="excavator-lt-komatsu-pc-200.html">Excavator L&amp;T Komatsu PC-200</a></li>
+              <li><a href="excavator-lt-komatsu-pc-200-long-boom.html">Excavator L&amp;T Komatsu PC-200 Long Boom</a></li>
+              <li><a href="vibratore-roller.html">Vibratore Roller</a></li>
+              <li><a href="pevar-machine.html">Pevar Machine</a></li>
+              <li><a href="excavator-cum-rock-breaker-tata-ex-200.html">Excavator cum Rock Breaker Tata Ex-200</a></li>
+              <li><a href="excavator-tata-ex-120.html">Excavator Tata Ex-120</a></li>
+              <li><a href="excavator-cum-loader-jcb.html">Excavator cum Loader JCB (3DX)</a></li>
+              <li><a href="hyva-tata.html">Hyva Tata (600cft)</a></li>
+              <li><a href="rmc-concrete-mixer.html">Concrete Mixer</a></li>
+              <li><a href="bharat-benz-1623c.html">Bharat Benz 1623C</a></li>
+              <li><a href="tractor-with-hydraulic-trolly.html">Tractor with Hydraulic Trolly</a></li>
+              <li><a href="tractor-with-scraper.html">Tractor with Scraper (for leveling)</a></li>
+              <li><a href="tractor-with-water-tanker.html">Tractor with Water Tanker</a></li>
+              <li><a href="tractor-with-hydraulic-compressor.html">Tractor with Hydraulic Compressor</a></li>
+            </ul>
+          </li>
+          </li>
+          <li style="z-index: 91;"><a class="" href="demolition-services.html">Demolition Service</a></li>
+          <li style="z-index: 91;"><a class="" href="services-available.html">Services Available</a></li>
+          <li style="z-index: 91;"><a class="" href="key-personnel.html">Key Personnel</a></li>
+          <li style="z-index: 91;"><a class="" href="enquiry.html">Enquiry</a></li>
+          <li style="z-index: 91;"><a class="" href="contactus.html" >Contact Us</a></li>
+        </ul>
+      </div></td>
+    </tr>
+  </table>
+</div>
+</div>
