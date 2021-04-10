6f0622226e1d005a392b2bde704125a36407233f
ganeshku73 <ganeshku73@gmail.com>
2021-04-10 12:11:49 +0530

    baibhav

---
+++ enquiry.php
@@ -0,0 +1,208 @@
+<?php include 'includes/_header.php';?>
+
+<div class="bg">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="21%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td class="enquiry-bg"><table width="95%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td height="25">&nbsp;</td>
+            </tr>
+            
+            
+            <tr>
+              <td><form action="submit.php" method="post" name="frm" id="frm2">
+                <table class="form-text" border="0" cellpadding="6" cellspacing="0" width="90%" align="center">
+                  <tbody>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Name*:</span></td>
+                      <td align="left"><input name="name" class="field-text1" id="name" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">City*:</span></td>
+                      <td align="left"><input name="city" class="field-text1" id="city" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Mobile*:</span></td>
+                      <td align="left"><input name="mobile2" class="field-text1" id="mobile" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Email*:</span></td>
+                      <td align="left"><input name="email" class="field-text1" id="email" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Query*:</span></td>
+                      <td align="left"><textarea name="query" rows="1" class="field-text1" id="query" style="width: 100px;"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2" align="center" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="82%">
+                        <tbody>
+                          <tr>
+                            <td align="left" valign="top" width="60%"><div align="center">
+                              <input name="btnSendFeedback" value="SUBMIT" id="btnSendFeedback3" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" onclick="return check_empty();" type="submit" />
+                            </div></td>
+                            <td align="left" valign="top" width="40%"><input name="btnSendFeedback" value="RESET" id="btnSendFeedback4" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" type="reset" /></td>
+                          </tr>
+                        </tbody>
+                      </table></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+        
+        
+      </table></td>
+      <td width="79%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td height="244"><div align="center"><div id="slideshow"><img src="images/banner-1.jpg" alt="Slideshow Image 1" width="790" height="274" class="active" /><img src="images/banner-2.jpg"><img src="images/banner-3.jpg"/><img src="images/banner-5.jpg"/><img src="images/banner-7.jpg"/><img src="images/banner-8.jpg"/>
+    
+</div>
+          </div></td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+        </tr>
+        
+        
+      </table></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td><table width="98%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td height="30" class="wel"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td width="33%">Enquiry</td>
+              <td width="36%">&nbsp;</td>
+              <td width="31%"><img src="images/rana1.gif" width="325" height="28" border="0" usemap="#Map" /></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td height="30"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td width="34%" valign="top"><img src="images/enquiry.jpg" width="267" height="294" /></td>
+              <td width="1%">&nbsp;</td>
+              <td width="65%"><form action="submit1.php" method="post" name="frm" id="frm">
+                <table width="90%" border="0" align="left" cellpadding="3" cellspacing="0" bgcolor="#EEEEEE">
+                  <tbody>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="middle" bgcolor="#FFFFFF" class="font"><b>Name</b></td>
+                      <td width="6%" valign="middle" bgcolor="#FFFFFF"><div align="center"><b><font size="2">:</font></b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><input name="name" id="name" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 150px;" type="text" /></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="middle" bgcolor="#FFFFFF" class="font">&nbsp;</td>
+                      <td valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="middle" bgcolor="#FFFFFF" class="font"><b>E-mail</b></td>
+                      <td width="6%" valign="middle" bgcolor="#FFFFFF"><div align="center"><b>:</b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><input name="email" id="txttitle2" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 150px;" type="text" /></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="middle" bgcolor="#FFFFFF" class="font">&nbsp;</td>
+                      <td valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="middle" bgcolor="#FFFFFF" class="font"><b>Address</b></td>
+                      <td width="6%" valign="middle" bgcolor="#FFFFFF"><div align="center"><b><font size="2">:</font></b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><textarea name="address" rows="6" id="address" cols="" style="border: 1px solid rgb(102, 102, 102); font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 200px;"></textarea></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="middle" bgcolor="#FFFFFF" class="font">&nbsp;</td>
+                      <td valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="middle" bgcolor="#FFFFFF" class="font"><b>Telephone</b></td>
+                      <td width="6%" valign="middle" bgcolor="#FFFFFF"><div align="center"><b>:</b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><input name="phone" id="txttitle5" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 150px;" type="text" /></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="middle" bgcolor="#FFFFFF" class="font">&nbsp;</td>
+                      <td valign="middle" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="middle" bgcolor="#FFFFFF" class="font"><b>Mobile</b></td>
+                      <td width="6%" valign="middle" bgcolor="#FFFFFF"><div align="center"><b>:</b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><input name="mobile" id="txttitle6" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 150px;" type="text" /></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td colspan="3" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="top" bgcolor="#FFFFFF" class="font"><b> Requirements</b></td>
+                      <td width="6%" valign="top" bgcolor="#FFFFFF"><div align="center"><b>:</b></div></td>
+                      <td width="58%" bgcolor="#FFFFFF"><textarea name="requirement" rows="6" id="requirement" cols="" style="border: 1px solid rgb(102, 102, 102); font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt; width: 200px;"></textarea></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td width="36%" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td width="6%" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td width="58%" bgcolor="#FFFFFF"><table border="0" cellpadding="0" cellspacing="0" width="60%">
+                        <tbody>
+                          <tr>
+                            <td align="left" valign="top" width="60%"><div align="center">
+                              <input name="submit" value="SUBMIT" id="btnSendFeedback" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" onclick="return check_empty();" type="submit" />
+                            </div></td>
+                            <td align="left" valign="top" width="40%"><input name="submit" value="RESET" id="btnSendFeedback2" style="border: 1px solid rgb(102, 102, 102); background-color: White; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" type="reset" /></td>
+                          </tr>
+                        </tbody>
+                      </table></td>
+                    </tr>
+                    <tr bgcolor="#FFFFFF">
+                      <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+                      <td bgcolor="#FFFFFF">&nbsp;</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td height="30">&nbsp;</td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <table width="98%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+     </tr>
+</table>
+<?php include 'includes/_footer.php';?>
+
+</div>
+
+</div>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="3,2,321,25" href="rana-pratap-group-companies.php" />
+</map>
+</body>
+</html>
---
+++ key-personnel.php
@@ -0,0 +1,224 @@
+<?php include 'includes/_header.php';?>
+
+<div class="bg">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="21%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td class="enquiry-bg"><table width="95%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td height="25">&nbsp;</td>
+            </tr>
+            
+            
+            <tr>
+              <td><form action="submit.php" method="post" name="frm" id="frm">
+                <table class="form-text" border="0" cellpadding="6" cellspacing="0" width="90%" align="center">
+                  <tbody>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Name*:</span></td>
+                      <td align="left"><input name="name" class="field-text1" id="name" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">City*:</span></td>
+                      <td align="left"><input name="city" class="field-text1" id="city" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Mobile*:</span></td>
+                      <td align="left"><input name="mobile" class="field-text1" id="mobile" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Email*:</span></td>
+                      <td align="left"><input name="email" class="field-text1" id="email" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Query*:</span></td>
+                      <td align="left"><textarea name="query" rows="1" class="field-text1" id="query" style="width: 100px;"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2" align="center" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="82%">
+                        <tbody>
+                          <tr>
+                            <td align="left" valign="top" width="60%"><div align="center">
+                              <input name="submit" value="SUBMIT" id="btnSendFeedback" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" onclick="return check_empty();" type="submit" />
+                            </div></td>
+                            <td align="left" valign="top" width="40%"><input name="submit" value="RESET" id="btnSendFeedback2" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" type="reset" /></td>
+                          </tr>
+                        </tbody>
+                      </table></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+        
+        
+      </table></td>
+      <td width="79%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td height="244"><div align="center"><div id="slideshow"><img src="images/banner-1.jpg" alt="Slideshow Image 1" width="790" height="274" class="active" /><img src="images/banner-2.jpg"><img src="images/banner-3.jpg"/><img src="images/banner-5.jpg"/><img src="images/banner-7.jpg"/><img src="images/banner-8.jpg"/>
+    
+</div>
+          </div></td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+        </tr>
+        
+        
+      </table></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td><table width="98%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td height="30" class="wel"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td width="33%">Key Personnel</td>
+              <td width="37%">&nbsp;</td>
+              <td width="30%"><img src="images/rana1.gif" width="325" height="28" border="0" usemap="#Map" /></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td height="30"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td><table width="100%" border="1" cellspacing="2" cellpadding="2">
+                <tr>
+                  <td height="30"><strong>S.No.</strong></td>
+                  <td height="30"><strong>NAME</strong></td>
+                  <td height="30"><strong>DESIGNATION</strong></td>
+                  <td height="30"><strong>QUALIFICATION</strong></td>
+                  <td><strong>EXPERIENCE</strong></td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>1.)</strong></td>
+                  <td height="30">Anil Kumar</td>
+                  <td height="30">Proprietor</td>
+                  <td height="30">Graduate</td>
+                  <td>15 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>2.)</strong></td>
+                  <td height="30">Lal Chand</td>
+                  <td height="30">Manager</td>
+                  <td height="30">D.C.E.</td>
+                  <td>25 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>3.)</strong></td>
+                  <td height="30">Rajesh Kumar</td>
+                  <td height="30">Manager</td>
+                  <td height="30">Graduate</td>
+                  <td>15 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>4.)</strong></td>
+                  <td height="30">Karan Kumar</td>
+                  <td height="30">Site Incharge</td>
+                  <td height="30">12th</td>
+                  <td>15 years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>5.)</strong></td>
+                  <td height="30">Santosh Kumar </td>
+                  <td height="30">Project Manager</td>
+                  <td height="30">D.C.E.</td>
+                  <td>15 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>6.}</strong></td>
+                  <td height="30">Manoj Kumar</td>
+                  <td height="30">Site Incharge</td>
+                  <td height="30">Graduate</td>
+                  <td>10 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>7.)</strong></td>
+                  <td height="30">Mayank Singh </td>
+                  <td height="30">Project Manager</td>
+                  <td height="30">D.C.E.</td>
+                  <td>07 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>8.)</strong></td>
+                  <td height="30">Sameer Chand Rathore</td>
+                  <td height="30">Maintenance Manager</td>
+                  <td height="30">12th </td>
+                  <td>15 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>9.)</strong></td>
+                  <td height="30">Shishank Singh</td>
+                  <td height="30">Supervisor</td>
+                  <td height="30">10th</td>
+                  <td>04 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>10.)</strong></td>
+                  <td height="30">Amit Kumar</td>
+                  <td height="30">Supervisor</td>
+                  <td height="30">10th</td>
+                  <td>03 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>11.)</strong></td>
+                  <td height="30">Arvind Chaudhary</td>
+                  <td height="30">Account Manager</td>
+                  <td height="30">B.Com</td>
+                  <td>12 Years</td>
+                </tr>
+                <tr>
+                  <td height="30"><strong>12.)</strong></td>
+                  <td height="30">Anshu Bhatia</td>
+                  <td height="30">Finance Manager</td>
+                  <td height="30">B.Com</td>
+                  <td>10 Years</td>
+                </tr>
+              </table></td>
+            </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td height="20">&nbsp;</td>
+        </tr>
+        <tr>
+          <td height="20"><strong>NOTE : </strong></td>
+        </tr>
+        <tr>
+          <td height="20"><strong>1.)</strong> We have separate staff for stores and accounts at our different sites.</td>
+        </tr>
+        <tr>
+          <td height="20"><strong>2.)</strong> We have sufficient no. of supervisory staff, skilled and unskilled labour force at our different sites.</td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <table width="98%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+     </tr>
+</table>
+<?php include 'includes/_footer.php';?>
+
+</div>
+</div>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="7,3,318,25" href="rana-pratap-group-companies.html" />
+</map>
+</body>
+</html>
