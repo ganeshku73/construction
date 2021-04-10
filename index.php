6da9f178e5783381c1fbd50650e1edffb8046a0b
ganeshku73 <ganeshku73@gmail.com>
2021-04-10 10:56:44 +0530

    sdsd

---
+++ index.php
@@ -0,0 +1,310 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
+<script type="text/javascript">
+
+function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+</script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script language="javascript" type="text/javascript" src="jquery_002.js"></script>
+<script src="swfobject_modified.js" type="text/javascript"></script>
+
+<link href="main.css" rel="stylesheet" type="text/css">
+<script src="pop_box.js" type="text/javascript"></script><script type="text/javascript" src="png.js"></script><link rel="stylesheet" type="text/css" href="ddsmoothmenu.css"><script type="text/javascript" src="jquery.js"></script><script type="text/javascript" src="ddsmoothmenu.js">
+
+</script><script type="text/javascript">
+
+ddsmoothmenu.init({
+mainmenuid: "smoothmenu1", //menu DIV id
+orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+classname: 'ddsmoothmenu', //class added to menu's outer DIV
+//customtheme: ["#1c5a80", "#18374a"],
+contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+</script><script type="text/jscript">
+function changeImage(filename)
+{
+	document.getElementById('blank').src = filename;
+}
+</script><script type="text/javascript">
+<!--
+function calc()
+{
+  //find the height of the internal page
+  var the_height1=
+    document.getElementById('blank').contentWindow.
+      document.body.scrollHeight;
+  //change the height of the iframe
+  document.getElementById('blank').height=
+      the_height1;
+}
+//-->
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Welcome To. M/s. Rana Pratap Construction & Earth Movers in Delhi | Earth Equipment in Delhi NCR| Rapid Building Demolition Rock Breaking Concrete Breaking Excavation | Dismantling in Delhi | Excavators Hiring Company in Delhi| M/s. Rana Pratap Construction 9871797901 in Delhi | Excavators Hire in Delhi | Rock Excavation Excavators Hiring in Delhi | M/s. Rana Pratap Construction & Earth Movers in Gurgaon | M/s. Rana Pratap Construction in delhi Faridabad | Earth Movers Machinery in Delhi NCR | M/s. Rana Pratap Construction in India| Excavators Hiring Company in India | Hard Rock Excavators Hiring Company in Faridabad | M/s. Rana Pratap Construction Hard Rock Excavation Company in Delhi | Earth Moving Machines in Delhi | M/s. Rana Pratap Construction & Earthmoving in Delhi | M/s. Rana Pratap Construction & Earthmoving and Hiring in Ghaziabad | M/s. Rana Pratap Construction & Earth Excavation Company in Delhi | Construction Equipment hiring Suppliers of India | Rental Construction Equipment Company | Rental Construction Equipment Company in Delhi | Rental Construction Equipment Company India | Earth Moving Machinery on Hire Basis | M/s. Rana Pratap Construction & Building Demolition in Delhi | M/s Rana Pratap Construction & Earth Movers in Uttranchal, Uttrakhand | M/s. Rana Pratap Construction Company in Uttrakhand | Construction & Earth Movers in Delhi | Constructions and Earth Movers | Hiring of Constructions Equipments | M/s. Rana Pratap Construction & Earthworks in Excavation of Soil in Delhi | Soft Rock Excavation in Delhi | Hard Rock Excavation in Delhi | Land Levelling filling compaction Disposal in Delhi NCR | Earthmoving Equipments on Rent in Gurgaon | Earth moving Equipments on Rent in Gurgaon | Earth moving Equipments on Rent in Delhi | Earth Moving Equipments on Rent in Noida | Earthmovers Equipments on Rent in Noida | Earthmovers Equipments on Rent in Delhi | Earthmovers Equipments on Rent in Gurgaon | Contractors earthmoving Machines in Delhi | Construction Earthmoving Machine in New Delhi | Tata EX200 Equipment Hire in Delhi NCR | Construction and Earthmoving Machinery in Delhi | M/s. Rana Pratap Constructions & Equipment Hiring Company In Delhi NCR India | Road Construction Equipments On Hire in Delhi | M/s Rana Pratap Construction Equipment Rental in Delhi India | Earth Mover and Excavators in Delhi | Earth Work and Excavators Company in Delhi | Excavators Hiring Company in Noida | Excavators Hiring Company in Gurgaon | Excavator Hiring in Gurgaon | Earth Movers Hiring in Gurgaon | Civil Engineering in Delhi | Building and Boundary Wall Construction Company in Jaipur | Agriculture Equipment in Chandigarh | Civil Contractors & Earthmoving and | All Type of Building Dismantling & Construction Company in Delhi | Rock Breaker Hiring in Delhi India | Hard Rock Work in Delhi | Poclain Dumpers & Hyva and Loaders Hiring in Delhi | Earth Disposal work in Delhi | land Laveling and Land Scraping Work in Delhi | Rock Breaking Concrete Breaking Excavation in Delhi | Hiring in Paschim Vihar New Delhi Delhi | Road Cutting and Construction Contractors in Delhi NCR | M/s. Rana Pratap Construction & Earth Movers Company In Delhi | Shifting of Earth in Delhi NCR | Rock Breaker Excavators Hiring in Delhi | Earth Movers and Excavators in Delhi Gurgaon | Excavators Hiring Company in Gurgaon Delhi NCR | Excavators Hiring in Gurgaon | Earth Movers Hiring in Gurgaon Noida | Earth Movers Machinery in Delhi Noida| Road Cutting and Construction Contractors in Delhi NCR | Hard rock Excavation in Himachal | Excavation Hiring Company in Himachal | Excavation Hiring Company in Uttaranchal | Earth moving contractors in Hardwar's | Earth moving contractors in Delhi Faridabad| Earth Moving Contractors in Gurgaon | Earth Moving Contractors in Uttaranchal | Earth Moving Contractors in Himachal | Civil Engineers and Contractors in Delhi</title>
+<link href="rana.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style3 {color: #CC9900}
+.style4 {color: #ececec}
+.style1 {font-weight: bold}
+.style2 {font-weight: bold}
+-->
+</style>
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+
+<div class="main">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="22">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div class="ddsmoothmenu" id="smoothmenu1">
+        <ul>
+          <li style="z-index: 91;"><a class=""  href="index.html">Home</a></li>
+          <li style="z-index: 91;"><a class=""  href="company-profile.html">Company Profile</a></li>
+          <li style="z-index: 91;"><a class="" href="our-clients.html">Our Clients</a></li>
+          <li style="z-index: 98;"><a class=""  href="#">Available Equipments</a>
+            <ul style="display: none; top: 29px; visibility: visible; left: 0px; width: 193px;">
+            <li><a href="excavator-hyundai-r215l-with-rock-breaker.html">Excavator Hyundai R215L with Rock Breaker</a></li>
+            <li><a href="excavator-hyundai-r140l.html">Excavator Hyundai R140L</a></li>
+              <li><a href="excavator-lt-komatsu-pc-200.html">Excavator L&amp;T Komatsu PC-200</a></li>
+              <li><a href="excavator-lt-komatsu-pc-200-long-boom.html">Excavator L&amp;T Komatsu PC-200 Long Boom</a></li>
+              <li><a href="vibratore-roller.html">Vibratore Roller</a></li>
+              <li><a href="pevar-machine.html">Pevar Machine</a></li>
+              <li><a href="excavator-cum-rock-breaker-tata-ex-200.html">Excavator cum Rock Breaker Tata Ex-200</a></li>
+              <li><a href="excavator-tata-ex-120.html">Excavator Tata Ex-120</a></li>
+              <li><a href="excavator-cum-loader-jcb.html">Excavator cum Loader JCB (3DX)</a></li>
+              <li><a href="hyva-tata.html">Hyva Tata (600cft)</a></li>
+              <li><a href="rmc-concrete-mixer.html">Concrete Mixer</a></li>
+              <li><a href="bharat-benz-1623c.html">Bharat Benz 1623C</a></li>
+              <li><a href="tractor-with-hydraulic-trolly.html">Tractor with Hydraulic Trolly</a></li>
+              <li><a href="tractor-with-scraper.html">Tractor with Scraper (for leveling)</a></li>
+              <li><a href="tractor-with-water-tanker.html">Tractor with Water Tanker</a></li>
+              <li><a href="tractor-with-hydraulic-compressor.html">Tractor with Hydraulic Compressor</a></li>
+            </ul>
+          </li>
+          </li>
+          <li style="z-index: 91;"><a class="" href="demolition-services.html">Demolition Service</a></li>
+          <li style="z-index: 91;"><a class="" href="services-available.html">Services Available</a></li>
+          <li style="z-index: 91;"><a class="" href="key-personnel.html">Key Personnel</a></li>
+          <li style="z-index: 91;"><a class="" href="enquiry.html">Enquiry</a></li>
+          <li style="z-index: 91;"><a class="" href="contactus.html" >Contact Us</a></li>
+        </ul>
+      </div></td>
+    </tr>
+  </table>
+</div>
+</div>
+<div class="bg">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="21%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td class="enquiry-bg"><table width="95%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td height="25">&nbsp;</td>
+            </tr>
+            
+            
+            <tr>
+              <td><form action="submit.php" method="post" name="frm" id="frm">
+                <table class="form-text" border="0" cellpadding="6" cellspacing="0" width="90%" align="center">
+                  <tbody>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Name*:</span></td>
+                      <td align="left"><input name="name" class="field-text1" id="name" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">City*:</span></td>
+                      <td align="left"><input name="city" class="field-text1" id="city" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Mobile*:</span></td>
+                      <td align="left"><input name="mobile" class="field-text1" id="mobile" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Email*:</span></td>
+                      <td align="left"><input name="email" class="field-text1" id="email" style="width: 100px;" type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td align="left" valign="top"><span class="style2">Query*:</span></td>
+                      <td align="left"><textarea name="query" rows="1" class="field-text1" id="query" style="width: 100px;"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2" align="center" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="82%">
+                        <tbody>
+                          <tr>
+                            <td align="left" valign="top" width="60%"><div align="center">
+                              <input name="submit" value="SUBMIT" id="btnSendFeedback" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" onclick="return check_empty();" type="submit" />
+                            </div></td>
+                            <td align="left" valign="top" width="40%"><input name="submit" value="RESET" id="btnSendFeedback2" style="border: 1px solid rgb(102, 102, 102); background-color:#1C254E; color:#FFFFFF; font-family: Verdana,Arial,Helvetica,sans-serif; font-size: 8pt;" type="reset" /></td>
+                          </tr>
+                        </tbody>
+                      </table></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form></td>
+            </tr>
+          </table></td>
+        </tr>
+        
+        
+      </table></td>
+      <td width="79%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td height="244"><div align="center"><div id="slideshow"><img src="images/banner-1.jpg" alt="Slideshow Image 1" width="790" height="274" class="active" /><img src="images/banner-2.jpg"><img src="images/banner-3.jpg"/><img src="images/banner-5.jpg"/><img src="images/banner-7.jpg"/><img src="images/banner-8.jpg"/>
+    
+</div>
+          </div></td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+        </tr>
+        
+        
+      </table></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td><div id="main-part">
+<div id="our-products">
+<div class="our-products"><img src="images/products.png" width="218" height="35" /></div>
+<div class="our-products"><img src="images/left-pic.jpg" width="218" height="224" /><br /><br /></div>
+<div class="our-products">
+  
+<div class="our-products"><img src="images/our-clients.jpg" width="218" height="35" /></div>
+<div class="our-products"><table width="92%" border="0" align="center" cellpadding="0" cellspacing="0">
+                                <tr>
+                                  <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                  <td><div align="justify" class="text"><span style="font-weight: bold">
+                                      <marquee direction="up" scrollamount="2" style="height:160px; cursor:pointer" onmouseover="this.setAttribute('scrollamount', 0, 0);" onmouseout="this.setAttribute('scrollamount', 1, 0);">
+                                      <img src="images/client-1.jpg" width="198" height="79" />&nbsp;<img src="images/client-2.jpg" width="198" height="79" />&nbsp;<img src="images/client-3.jpg" width="198" height="79" />&nbsp;<img src="images/client-4.jpg" width="198" height="79" />&nbsp;<img src="images/client-5.jpg" width="198" height="79" />&nbsp;<img src="images/client-6.jpg" width="198" height="79" />&nbsp;<img src="images/client-7.jpg" width="198" height="79" />&nbsp;<img src="images/client-8.jpg" width="198" height="79" />&nbsp;<img src="images/client-9.jpg" width="198" height="79" />
+                                      </marquee>
+                                  </span></div></td>
+                                </tr>
+                                <tr>
+                                  <td>&nbsp;</td>
+                                </tr>
+                            </table>
+</div>
+<div class="our-products"><img src="images/client-bottom.jpg" width="218" height="13" /></div>
+</div>
+</div>
+<div id="welcome">
+<div class="welcome"><img src="images/welcome.jpg" width="532" height="47" /><img src="images/rana.gif" width="193" height="47" border="0" usemap="#Map" /></div>
+<div class="welcome">
+  <p>We avail this opportunity to introducing ourselves as one of leading civil Contractor specialized in<strong> Earthwork in excavation and Earth Filling, Rock excavation, building demolishing Work and Road Construction Project.</strong> Serving Various esteemed organization since last 18 years. </p>
+  <p>We are one of the leading professionals, fully equipped with latest Construction machinery as required for <strong>Excavation, leveling, breaker, Dressing, compaction etc</strong>. &amp; having the team of qualified and experience Managers professinals and civil Engineers to deliver the right quality, Workmanship as well versed with most modern construction techniques to Suit the<strong> project requirement safety and time schedule</strong>.</p>
+  <p>We take up complete responsibility of project management, which Includes execution and coordination. It will be our endeavor to complete the Project in lowest possible time frame with safety and least amount of funds Input of owner. It would be a matter of privilege for us to work your organization Our Services will be with total commitment and declaration.</p>
+</div>
+<div class="welcome"><img src="images/dot-line.png" width="728" height="1" /></div>
+<div class="welcome">&nbsp;</div>
+<div class="welcome">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="44%"><img src="images/demolition-services.png" width="322" height="190" /></td>
+      <td width="2%">&nbsp;</td>
+      <td width="54%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td><img src="images/demolition.png" width="189" height="26" /></td>
+        </tr>
+        <tr>
+          <td><strong>Old Building Demolition Services</strong><br />
+            Rana Pratap Construction we are also engaged in undertaking demolition, demolishing and dismantling projects. Our highly qualified and experienced professionals that are fully trained and hold the capability to handle all types of demolition / dismantling / demolishing jobs. In these services, we carry out recycling of material and complete removal of buildings &amp; partial demolition of all types of structures or plant &amp; machinery from small houses for private sectors to large industrial or residential site.</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>
+</div>
+<div class="welcome"><img src="images/dot-line.png" width="728" height="1" /></div>
+<div class="welcome">&nbsp;</div>
+<div class="welcome"><img src="images/earthmover_machines.png" width="728" height="105" /></div>
+</div>
+
+</div></td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <table width="98%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+     </tr>
+</table>
+</div>
+<div class="bg">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td height="25" bgcolor="#3a3a3a"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="38%"><div align="center" class="style4">Copyright 2013 Rana Pratap Construction. All Rights Reserved.&nbsp;&nbsp;</div></td>
+          <td width="36%">&nbsp;</td>
+          <td width="26%" height="22">&nbsp;</td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>
+</div>
+</div>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="7,4,187,41" href="rana-pratap-group-companies.html" />
+</map>
+</body>
+</html>
